2f234fe3a4493a4ff9960deb21d4ab4e53252519
Pascal Roger J T <91580977+Pascalrjt@users.noreply.github.com>
2023-09-11 09:17:49 +0700

    Update djikstra.py

--- djikstra.py
+++ djikstra.py
@@ -17,12 +17,12 @@ def dijkstra(graph, start, end):
         # If we've reached the end node, reconstruct and return the path
         if current_node == end:
             path = []
-            total_cost = 0
+            cost = []
             while current_node:
                 path.append(current_node)
-                total_cost += distances[current_node]
+                cost.append(distances[current_node])
                 current_node = previous.get(current_node)
-            return total_cost, list(reversed(path))
+            return distances[end], list(reversed(path)), list(reversed(cost))
         
         # If we've already processed this node, skip it
         if current_distance > distances[current_node]:
@@ -43,7 +43,6 @@ def dijkstra(graph, start, end):
 # Example usage:
 if __name__ == "__main__":
     # Define the graph as an adjacency dictionary
-
     graph = {
             'A': {'B': 1, 'C': 3, 'D': 5},
             'B': {'A': 1, 'E': 7, 'F': 4},
@@ -59,15 +58,15 @@ if __name__ == "__main__":
             'L': {'G': 1, 'M': 3},
             'M': {'H': 4, 'I': 7, 'L': 3}
     }
-    
     start_node = 'A'
     end_node = 'M'
     
     result = dijkstra(graph, start_node, end_node)
     
     if result:
-        total_cost, shortest_path = result
-        print(f'Total cost from {start_node} to {end_node}: {total_cost}')
+        shortest_distance, shortest_path, node_costs = result
+        print(f'Shortest distance from {start_node} to {end_node} is {shortest_distance}')
         print(f'Shortest path: {" -> ".join(shortest_path)}')
+        print(f'Cost of nodes traversed: {node_costs}')
     else:
         print(f'No path from {start_node} to {end_node} found.')
